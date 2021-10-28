224010c86f125993940d400d246d04d0409eea15
IIIT <reddy.khoushik@gmail.com>
2021-10-28 15:20:08 +0530

    serverless branch

--- src/index.tsx
+++ src/index.tsx
@@ -22,7 +22,8 @@ import "./theme/global.css";
 // import DemoSwitcher from 'components/DemoSwitcher/SwitcherBtn';
 
 const httpLink = new HttpLink({
-  uri: "http://localhost:4000/custom/graphql"
+  // uri: "http://localhost:4000/custom/graphql",
+  uri:"https://11xy1zsw0k.execute-api.ap-south-1.amazonaws.com/dev/custom"
 });
 
 const errorLink = onError(({ graphQLErrors, networkError }) => {
